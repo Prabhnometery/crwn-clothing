38a19997c06479e8b1488c5cdc6a3b51b6a9ebb1
Prabh Simran Singh Badwal <61562651+Prabhnometery@users.noreply.github.com>
2020-10-13 15:22:12 -0400

    updated shop data with new data, updated card utils to group items in a cart

--- src/redux/cart/card.reducer.js
+++ src/redux/cart/card.reducer.js
@@ -1,5 +1,8 @@
+import { addItemToCart } from './cart.utils'; 
+
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 }
 
 const cardReducer = (state = INITIAL_STATE, action) => {
@@ -8,7 +11,11 @@ const cardReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 hidden: !state.hidden // also we can write action.payload
-
+            };
+        case 'ADD_ITEM':
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload)  //Spreading in the array values
             }
             default:
                 return state;
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,3 +1,9 @@
 export const toggleCartHidden = () => ({
     type: 'TOGGLE_CART_HIDDEN'
 });
+
+export const addItem = item => ({
+
+    type: 'ADD_ITEM',
+    payload: item  //Optional
+});
